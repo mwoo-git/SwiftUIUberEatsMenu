7fbf301232f79e03a2843b9ee733b35a26c6d2f6
WooMin <hanulbom@gmail.com>
2022-09-01 16:30:17 +0900

    Initial Commit

---
+++ SwiftUIUberEatsMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIUberEatsMenu
+//
+//  Created by Woo Min on 2022/09/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
