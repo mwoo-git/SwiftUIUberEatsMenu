8ebe34810cb6a44862fec6eb3be636aaa69fa650
WooMin <hanulbom@gmail.com>
2022-09-03 02:20:52 +0900

    @Namespace matchedGeometryEffect를 사용해서 메뉴바가 자연스럽게 이동하도록 수정

--- SwiftUIUberEatsMenu/MenuOptionsList.swift
+++ SwiftUIUberEatsMenu/MenuOptionsList.swift
@@ -9,31 +9,40 @@ import SwiftUI
 
 struct MenuOptionsList: View {
     @Binding var selectedOption: MenuBarOptions //터치하면 작동
+    @Namespace var animation // 메뉴바 밑의 선이 순간이동 하던 것을 자연스럽게 바꾸기위함 (matchedGeometryEffect)
     
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 30) {
-                ForEach(MenuBarOptions.allCases, id: \.self) { item in
-                    VStack {
-                        Text(item.title)
-                            .foregroundColor(item == selectedOption ? .black : .gray)
-                        
-                        //움직이는 바
-                        if selectedOption == item {
-                            Capsule()
-                                .fill(.black)
-                                .frame(height: 3)
-                                .padding(.horizontal, -10)
-                        } else {
-                            Capsule()
-                                .fill(.clear)
-                                .frame(height: 3)
-                                .padding(.horizontal, -10)
+        ScrollViewReader { proxy in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(MenuBarOptions.allCases, id: \.self) { item in
+                        VStack {
+                            Text(item.title)
+                                .foregroundColor(item == selectedOption ? .black : .gray)
+                            
+                            //움직이는 바
+                            if selectedOption == item {
+                                Capsule()
+                                    .fill(.black)
+                                    .matchedGeometryEffect(id: "item", in: animation) //메뉴바 밑의 선 자연스럽게 이동
+                                    .frame(height: 3)
+                                    .padding(.horizontal, -10)
+                            } else {
+                                Capsule()
+                                    .fill(.clear)
+                                    .frame(height: 3)
+                                    .padding(.horizontal, -10)
+                            }
                         }
-                    }
-                    .onTapGesture {
-                        self.selectedOption = item
-                        // 터치하면 셀렉티드옵션 값을 아이템 값으로 바꿔줍니다.
+                        .onTapGesture {
+                            withAnimation(.easeInOut) {
+                                self.selectedOption = item
+                                // 터치하면 셀렉티드옵션 값을 아이템 값으로 바꿔줍니다.
+                                
+                                // 메뉴를 선택하면 예쁘게(?) 이동 할 수 있도록 해주는 코드
+                                proxy.scrollTo(item, anchor: .topTrailing)
+                            }
+                        }
                     }
                 }
             }
